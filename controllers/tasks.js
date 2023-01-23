3b2c4750ebce267d5a1332847afc309e77bf9e76
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-01-23 13:13:32 +0530

    initial commit - Node starter code

---
+++ controllers/tasks.js
@@ -0,0 +1,26 @@
+
+const getAllTasks = (req, res) => {
+    res.send('Get All Tasks');
+}
+
+const createTask = (req, res) => {
+    res.json(req.body);
+}
+const getTask = (req, res) => {
+    res.json({ id: req.params.id });
+}
+const updateTask = (req, res) => {
+    res.send('Update a Task');
+}
+const deleteTask = (req, res) => {
+    res.send('Delete a Task');
+}
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
