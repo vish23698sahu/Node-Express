9a4fd946c877fd4090c0826bc781d2d0e11c6de8
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-01-31 19:26:32 +0530

    v - 3 custom error handlers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
@@ -10,9 +12,10 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
@@ -21,6 +24,6 @@ const start = async () => {
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 start();
